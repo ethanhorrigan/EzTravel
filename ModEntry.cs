986e24bb1c0bf81a5d1ab3350600818306a94805
EthanHorrigan <ethanhorr@gmail.com>
2020-04-09 16:35:43 +0100

    Warp to Map Point

--- ModEntry.cs
+++ ModEntry.cs
@@ -28,11 +28,15 @@ namespace EzTravel
         {
             Config = helper.ReadConfig<ModConfig>();
             helper.Events.Input.ButtonPressed += this.OnButtonPressed;
-            helper.Events.Player.Warped += this.OnWarped;
-            helper.Events.Input.CursorMoved += this.OnCursorMoved;
         }
 
-        private void OnCursorMoved(object sender, CursorMovedEventArgs e) 
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
         {
             if (!Context.IsWorldReady)
                 return;
@@ -45,44 +49,24 @@ namespace EzTravel
             if (map == null)
                 return;
 
-
-        }
-        /*********
-        ** Private methods
-        *********/
-        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-            var menu = (Game1.activeClickableMenu as GameMenu);
-            if (menu == null || menu.currentTab != GameMenu.mapTab) // Also make sure it's on the right tab(Map)
-                return;
-
-            var map_page = (Helper.Reflection.GetField<List<IClickableMenu>>(menu, "pages").GetValue()[3]) as MapPage;
-            if (map_page == null)
-                return;
-
             int x = Game1.getMouseX();
             int y = Game1.getMouseY();
 
-            foreach(ClickableComponent point in map_page.points)
+            foreach (ClickableComponent point in map.points)
             {
                 if (!point.containsPoint(x, y))
                     continue;
-            }
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
-            //if(e.Button == SButton.LeftShift + SButton.B)
-        }
 
-        private void OnWarped(object sender, WarpedEventArgs e)
-        {
-            this.Monitor.Log($"{Game1.player.Name} warped to {e.NewLocation}.", LogLevel.Debug);
+                var loc = TravelUtils.GetLocationForMapPoint(point);
+                var travelPoint = TravelUtils.GetFastTravelPointForMapPoint(point);
+                this.Monitor.Log($"location: {loc.Name}.", LogLevel.Debug);
+                if (e.Button == SButton.MouseLeft)
+                {
+                    Game1.warpFarmer(travelPoint.RouteName == null ? loc.Name : travelPoint.RouteName, travelPoint.SpawnPoint.X, travelPoint.SpawnPoint.Y, false);
+                    Game1.exitActiveMenu();
+                }          
+            }
+
         }
     }
 }
